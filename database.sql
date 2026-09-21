27b96a186a7b4b99fee0afe242d5db18285cab6f
Rafael Castro <104697377+rafaelsantoscastro@users.noreply.github.com>
2026-09-21 14:28:42 -0300

    Create DataWarehouse database and schemas
    This script creates a new database named 'DataWarehouse' with three schemas: DW_BRONZE, DW_SILVER, and DW_GOLD.

---
+++ database.sql
@@ -0,0 +1,14 @@
+/*
+Create database and Schemas
+Script Purpose:
+  This is script create a new database named "DataWarehouse'
+
+
+*/
+
+CREATE SCHEMA DW_BRONZE;
+GO
+CREATE SCHEMA DW_SILVER;
+GO
+CREATE SCHEMA DW_GOLD;
+GO
